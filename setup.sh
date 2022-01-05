6281d116bd5dd41c302025510bd375567deec85c
Rahul Gupta-Iwasaki <rahul@every.org>
2022-01-04 18:11:13 -0800

    Add global setup file.

---
+++ setup.sh
@@ -0,0 +1,17 @@
+(
+  SCRIPT=$(realpath "$0")
+  # Absolute path this script is in, thus /home/user/bin
+  SCRIPTPATH=$(dirname "$SCRIPT")
+
+  echo "~~~~~~~~~~~~~~~~~"
+  echo "Setting up yabai"
+  echo "~~~~~~~~~~~~~~~~~"
+  "$SCRIPTPATH/yabai/setup.sh"
+  echo "\n"
+
+  echo "~~~~~~~~~~~~~~~"
+  echo "Setting up zsh"
+  echo "~~~~~~~~~~~~~~~"
+  "$SCRIPTPATH/zsh/setup.sh"
+  echo "\n"
+)
